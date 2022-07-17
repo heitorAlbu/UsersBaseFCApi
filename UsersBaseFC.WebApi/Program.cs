cc15ef271387a2f877696de5aefc36a1d0025206
heitorAlbu <heitor.albuquerque@gmail.com>
2022-07-17 16:03:04 -0300

    adicionando permissão de hearders no cors

--- UsersBaseFC.WebApi/Program.cs
+++ UsersBaseFC.WebApi/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddCors(options =>
        builder =>
        {
            builder.WithOrigins("http://localhost:4200")
+          .AllowAnyHeader()
           .AllowAnyMethod()
           .AllowAnyOrigin();
        });
